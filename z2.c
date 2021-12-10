36000b58b2b82d10c99c23178497a6983d1302cb
Romadariychuck <78026310+Romadariychuck@users.noreply.github.com>
2021-12-10 14:01:09 +0200

    Лабораторна №2

---
+++ z2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <ctype.h>
+int main(void) {
+setlocale(LC_CTYPE,"UA");
+char string;
+printf("Задайте рядок:  \n");
+scanf("%s",&string);
+    if(string == 'a' || string =='b' || string =='c' || string =='d' || string =='e' || string =='f' || string =='g' || string =='h' || string =='i' || string =='j' || string =='k' || string =='l' || string =='m' || string =='n' || string =='o' || string =='p' || string =='q' || string =='r' || string =='s' || string =='t' || string =='u' || string =='v' || string =='x' || string =='w' || string =='z'){printf("Ви задали символи Латинського алфавiту\n"); return 0;} //Умова на ізоляцію Латинської мови, якщо змінна sym дорівнює хоча б одному з заданих символів, то процес повертається назад 
+		else(string>0);{printf("Ви задали цифру...\n"); return 0;}
+     printf("Ваш рядок: %s \n",&string);
+return 0;
+}
